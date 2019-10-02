6e2237cf4146fe9a988d4c5bbcc2f6802e9d4663
ErlingLie <Erling.s.lie@gmail.com>
2019-10-02 18:35:16 +0200

    Cleanup of experimental data

--- FysikkProject/Plotting.py
+++ FysikkProject/Plotting.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
 import ReadFile
+
 from matplotlib import pyplot as plt
 
-def main():
-    t,x,y = ReadFile.readFromFile("..\H2Data.txt")
+def plotData(fileName):
+    t,x,y = ReadFile.readFromFile(fileName)
     for i in range(10):
         plt.plot(t[i],y[i])
 
     plt.show()
+    
+
+def main():
+    plotData("..\H1Data.txt")
+    plotData("..\H2Data.txt")
+    plotData("..\H3Data.txt")
+    plotData("..\H4Data.txt")
+    plotData("..\H3DataBehandlet.txt")
+    plotData("..\destFile.txt")
+    plotData("..\destination.txt")
     
 main()
---
+++ FysikkProject/tools.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import ReadFile
+import numpy as np
+def cleanData(string):
+    w = open(string,"w")
+    f = open("..\destination.txt","r")
+    for line in f:
+        for s in line.split():
+            val = 0
+            try:
+                val = float(s)
+            except:
+                lst = s.split("E")
+                val = float(lst[0])* 10**int(lst[1])
+            w.write(str(val).strip()+ "\t")
+        w.write("\n")
+    f.close()
+    return
+
+
+
+def timeShiftData(fileName):
+    t,x,y = ReadFile.readFromFile(fileName)
+    
+    for i in range(len(t)):
+        while(abs(y[i][1]-y[i][0]) < 0.000065):
+            x[i] = x[i][1:]
+            y[i] = y[i][1:]
+    f = open("..\destFile.txt", "w")
+    for i in range(len(y[0])):
+        for j in range(10):
+            if(i<len(y[j])):
+                f.write(str(t[j][i]) + " ")
+                f.write(str(x[j][i]) + " ")
+                f.write(str(y[j][i]) + "\t")
+            else:
+                f.write(str(t[0][i]) + " ")
+                f.write(str(0) + " ")
+                f.write(str(0) + "\t")
+        f.write("\n")
+            
+
+
+def fixData2():
+    t,x,y = ReadFile.readFromFile("..\H2Cleaned.txt")
+    for i in range(len(t)):
+        print(i)
+        for j in range(len(t[i])):
+            t[i][j] -= val
+            t[i][j] = round(t[i][j],2)
+    f = open("..\destination.txt","w")
+    for i in range(len(t[0])):
+        for j in range(10):
+            f.write(str(t[j][i]) + "\t")
+            f.write(str(x[j][i]) + "\t")
+            f.write(str(y[j][i]) + "\t\t")
+        f.write("\n")
+
+
+fixData2()
+
+
